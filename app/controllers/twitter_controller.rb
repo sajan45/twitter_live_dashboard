cb1c0ea10df3211585ff30af425b8c839aecba34
sajan <sajan.sahu@live.com>
2019-12-17 18:56:23 +0530

    added stream for REST API

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,49 @@
 class TwitterController < ApplicationController
+  include ActionController::Live
+
   def tweets
+    if request.format.json?
+      stream_tweet
+    end
+  end
+
+  private
+
+  # Please note that I used SSE for live streaming on REST API
+  # due to time constraints, but it is not a good approach for for blocking
+  # servers, as every request will block a thread or process causing requese exhausation.
+  # A better approached would be using some event based servers like the 'eventmachine'
+  def stream_tweet
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = SSE.new(response.stream)
+    begin
+      stream_client = TwitterFactory.new_streaming_client
+      topic = params[:source]
+      filter_options = {}
+      if topic.present?
+        if topic[0] == '@'
+          rest_client = TwitterFactory.new_rest_client
+          user = rest_client.user(topic[1..-1])
+          filter_options[:follow] = user.id
+        elsif topic[0] == '#'
+          filter_options[:track] = topic # may need to remove '#'
+        end
+        stream_client.filter(filter_options) do |object|
+          if object.is_a?(Twitter::Tweet)
+            tweet = object.to_hash
+            p tweet
+            sse.write(tweet)
+          end
+        end
+      else
+        sse.write({error: "No topic"})
+      end
+    rescue IOError
+    rescue ClientDisconnected
+      # Client Disconnected
+    ensure
+      sse.close
+    end
+    render nothing: true
   end
 end
